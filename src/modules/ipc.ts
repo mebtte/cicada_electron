a020dd07ad577fa6732e41146c8b9cc2767d45fc
mebtte <hi@mebtte.com>
2021-02-21 15:20:38 +0800

    ipc add channel

--- src/modules/ipc.ts
+++ src/modules/ipc.ts
@@ -3,10 +3,14 @@ import { ipcMain, app, shell } from 'electron';
 import { ORIGIN } from '../constants/regexp';
 import store, { Key } from '../platform/store';
 import configWindow from './config_window';
+import playerWindow from './player_window';
 
 enum Channel {
   RELAUNCH = 'relaunch', // 重启 APP
   OPEN_LINK = 'open_link', // 打开链接
+
+  MINIMIZE_PLAYER_WINDOW = 'minimize_player_window', // 最小化播放器窗口
+  HIDE_PLAYER_WINDOW = 'hide_player_window', // 隐藏播放器窗口
 
   CLOSE_CONFIG_WINDOW = 'close_config_window', // 关闭配置窗口
 
@@ -21,6 +25,9 @@ ipcMain.handle(Channel.RELAUNCH, () => {
 ipcMain.handle(Channel.OPEN_LINK, (_, { link }: { link: string }) =>
   shell.openExternal(link)
 );
+
+ipcMain.handle(Channel.MINIMIZE_PLAYER_WINDOW, () => playerWindow.minimize());
+ipcMain.handle(Channel.HIDE_PLAYER_WINDOW, () => playerWindow.hide());
 
 ipcMain.handle(Channel.CLOSE_CONFIG_WINDOW, () => configWindow.close());
 
